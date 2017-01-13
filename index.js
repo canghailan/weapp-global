4824a17894a4b04ad136f38c226b89471be8e96a
huhao <415328529@qq.com>
2017-01-13 19:23:04 +0800

    初始化

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "Array": Array,
+    "Date": Date,
+    "Error": Error,
+    "Function": Function,
+    "Math": Math,
+    "Object": Object,
+    "RegExp": RegExp,
+    "String": String,
+    "TypeError": TypeError,
+    "setTimeout": setTimeout,
+    "clearTimeout": clearTimeout,
+    "setInterval": setInterval,
+    "clearInterval": clearInterval
+};
